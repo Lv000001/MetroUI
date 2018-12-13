69900c969cebe32968b767b89f2f929ba017a948
Lv000001 <38172846+Lv000001@users.noreply.github.com>
2018-12-13 21:54:54 +0800

    Add files via upload

---
+++ MetroUI/MetroUI/SettingsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetroUI
+{
+    /// <summary>
+    /// SettingsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ColorSelectorDialog color = new ColorSelectorDialog(Colors.CadetBlue);
+            color.ShowDialog();
+        }
+    }
+}
